ed99e9d48c34ca66217ffe16f463bca1464e907a
Povilas Kulevičius <kulevicius.povilas@gmail.com>
2019-01-08 10:14:34 +0200

    gelbekit

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,40 +13,47 @@ public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         String input, output,result;
-        while(true) {
-            System.out.println("c - encode");
-            System.out.println("d - decode");
-            System.out.println("e - exit");
-            Scanner in = new Scanner(System.in);
-            String choice = in.next();
 
-            switch (choice) {
-                case "c":
-                    System.out.println("dictionary_length input_file.txt output_file.txt");
-                    int length = in.nextInt();
-                    input = in.next();
-                    output = in.next();
-                    if (length == 0)
-                        length = 100;
-                    Encode enc = new Encode(input, output, length);
-                    enc.encode();
-                    break;
-                case "d":
-                    System.out.println("output_file.txt result_file.txt");
-                    output = in.next();
-                    result = in.next();
-                    Decode dec = new Decode();
-                    dec.decode(output, result);
-                    break;
-                case "e":
-                    System.exit(0);
+        Encode enc = new Encode("input.txt", "output.txt", 12);
+        enc.encode();
+        Decode dec = new Decode();
+        dec.decode("output.txt", "result.txt");
 
-                default:
-                    System.out.println("wrong input");
-                    break;
 
-            }
-        }
+//        while(true) {
+//            System.out.println("c - encode");
+//            System.out.println("d - decode");
+//            System.out.println("e - exit");
+//            Scanner in = new Scanner(System.in);
+//            String choice = in.next();
+//
+//            switch (choice) {
+//                case "c":
+//                    System.out.println("dictionary_length input_file.txt output_file.txt");
+//                    int length = in.nextInt();
+//                    input = in.next();
+//                    output = in.next();
+//                    if (length == 0)
+//                        length = 100;
+//                    Encode enc = new Encode(input, output, length);
+//                    enc.encode();
+//                    break;
+//                case "d":
+//                    System.out.println("output_file.txt result_file.txt");
+//                    output = in.next();
+//                    result = in.next();
+//                    Decode dec = new Decode();
+//                    dec.decode(output, result);
+//                    break;
+//                case "e":
+//                    System.exit(0);
+//
+//                default:
+//                    System.out.println("wrong input");
+//                    break;
+//
+//            }
+//        }
     }
 
 
